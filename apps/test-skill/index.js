d9754d0ded8085925a1805d18cb297c11ea8408a
Tien Hoang Ngoc <tienhn@asiantech.vn>
2017-05-16 16:23:05 +0700

    Add intent music

--- apps/test-skill/index.js
+++ apps/test-skill/index.js
@@ -1,42 +1,55 @@
 module.change_code = 1;
 'use strict';
 
-var alexa = require( 'alexa-app' );
-var app = new alexa.app( 'test-skill' );
+var alexa = require('alexa-app');
+var app = new alexa.app('test-skill');
 
 
-app.launch( function( request, response ) {
-	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
-} );
+app.launch(function(request, response) {
+    response.say('Welcome to your test skill').reprompt('Way to go. You got it to run. Bad ass.').shouldEndSession(false);
+});
 
 
-app.error = function( exception, request, response ) {
-	console.log(exception)
-	console.log(request);
-	console.log(response);
-	response.say( 'Sorry an error occured ' + error.message);
+app.error = function(exception, request, response) {
+    console.log(exception)
+    console.log(request);
+    console.log(response);
+    response.say('Sorry an error occured ' + error.message);
 };
 
-app.intent('sayNumber',
-  {
-    "slots":{"number":"NUMBER"}
-	,"utterances":[
-		"say the number {1-100|number}",
-		"give me the number {1-100|number}",
-		"tell me the number {1-100|number}",
-		"I want to hear you say the number {1-100|number}"]
-  },
-  function(request,response) {
-    var number = request.slot('number');
-    response.say("You asked for the number "+number);
-		var stream = {
-    "url": "http://api.mp3.zing.vn/api/mobile/source/song/LGJGTLGNQJGXEVGTLDJTDGLG",
-    "token": "some_token",
-    "expectedPreviousToken": "some_previous_token",
-    "offsetInMilliseconds": 0
-  };
-	response.audioPlayerPlayStream("REPLACE_ALL", stream);
-  }
+app.intent('sayNumber', {
+        "slots": {
+            "number": "NUMBER"
+        },
+        "utterances": [
+            "say the number {1-100|number}",
+            "give me the number {1-100|number}",
+            "tell me the number {1-100|number}",
+            "I want to hear you say the number {1-100|number}"
+        ]
+    },
+    function(request, response) {
+        var number = request.slot('number');
+        response.say("You asked for the number " + number);
+    }
+);
+
+app.intent('playMusic', {
+        "utterances": [
+            "play music",
+            "play some music",
+            "give me some song"
+        ]
+    },
+    function(request, response) {
+        var stream = {
+            "url": "http://api.mp3.zing.vn/api/mobile/source/song/LGJGTLGNQJGXEVGTLDJTDGLG",
+            "token": "some_token",
+            "expectedPreviousToken": "some_previous_token",
+            "offsetInMilliseconds": 0
+        };
+        response.audioPlayerPlayStream("REPLACE_ALL", stream);
+    }
 );
 
 module.exports = app;
